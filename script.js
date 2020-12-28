922a4c07d76ae56adc1d8a56a11aa8979c6e5289
Maz <m.balandari.toroghi@gmail.com>
2020-12-28 19:12:04 +0300

    all pics

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var modal = document.getElementById('myModal');
-var myIMG = ['myimg1', 'myimg2', 'myimg3', 'myimg4', 'myimg5','myimg6','myimg7','myimg8','myimg9','myimg10','myimg11', 'myimg12','myimg13','myimg14','myimg15', 'myimg16']
+var myIMG = ['myimg1', 'myimg2', 'myimg3', 'myimg4', 'myimg5','myimg6','myimg7','myimg8','myimg9','myimg10','myimg11', 'myimg12','myimg13','myimg14','myimg15', 'myimg16', 'myimg17', 'myimg18', 'myimg19', 'myimg20', 'myimg21', 'myimg22', 'myimg23']
     for (var i = 0; i < myIMG.length; i++) {
         var img = document.getElementById(myIMG[i]);
         var modalImg = document.getElementById("img01");
